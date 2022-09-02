390376d78490a775ef59ba0fd078def14a4c8c85
Brian Krause <briankrause.py@gmail.com>
2022-09-02 13:57:15 -0700

    Initial Commit

---
+++ BetterRest1/BetterRest1App.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRest1App.swift
+//  BetterRest1
+//
+//  Created by Brian Krause on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest1
+//
+//  Created by Brian Krause on 9/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
